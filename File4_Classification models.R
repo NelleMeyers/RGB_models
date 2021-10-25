18a64b4c60a2075082917d07120b8bdfa6235c27
NelleMeyers <91480846+NelleMeyers@users.noreply.github.com>
2021-10-25 14:42:55 +0200

    Update File4_Classification models.R

--- File4_Classification models.R
+++ File4_Classification models.R
@@ -32,17 +32,19 @@ validationdata<-create_train_validate(RGB_dataset,size = 0.8, train = FALSE)
 #adjust 'minbucket' (minimum amount of records that end up in an end node),
 #and adjust 'cp' (minimum model fit improvement required for splits not to be discarded).
 
+#PDM
 PDM_creation <- function(Overall_dataset) {
   RGB_train_data<-create_train_test(Overall_dataset, size = 0.8, train=TRUE)
-  PDM_model <- rpart(Material ~ .,
+  PDM_model <- rpart(Particle_type ~ .,
                      data = RGB_train_data, method = "class",
                      control =rpart.control(minsplit=8,minbucket=3, cp=0,02))
   return(PDM_model)
 }
 
+#PIM
 PIM_creation <- function(Overall_dataset) {
   RGB_train_data<-create_train_test(Overall_dataset, size = 0.8, train=TRUE)
-  PIM_model <- rpart(Material ~ .,
+  PIM_model <- rpart(Particle_type ~ .,
                      data = RGB_train_data, method = "class",
                      control =rpart.control(minsplit=39,minbucket=13, cp=0,02))
   return(PIM_model)
@@ -52,12 +54,14 @@ PDM_creation(RGB_dataset)
 PIM_creation(RGB_dataset)
 
 #Function to visualise your PDM or PIM model
+
+#PDM
 PDM_visualisation <-function(Overall_dataset){
   PDM_model_plotted <- fancyRpartPlot(PDM_creation(Overall_dataset), caption = NULL)
   return(PDM_model_plotted)
 }
 
-
+#PIM
 PIM_visualisation <-function(Overall_dataset){
   PIM_model_plotted <- fancyRpartPlot(PIM_creation(Overall_dataset), caption = NULL)
   return(PIM_model_plotted)
@@ -67,22 +71,25 @@ PDM_visualisation(RGB_dataset)
 PIM_visualisation(RGB_dataset)
 
 #Function to validate your PDM or PIM model using your validation datasets.
+
+#PDM
 PDM_validation<-function(Overall_dataset){
   RGB_validation_data<-create_train_test(Overall_dataset, size = 0.8, train=FALSE)
   PDM_RGB_model<-PDM_creation(Overall_dataset)
   predict_unknown<-predict(PDM_RGB_model, RGB_validation_data, type="class")
   A <-as.factor(predict(PDM_RGB_model, RGB_validation_data, type="class"))
-  B <-as.factor(RGB_validation_data$Material)
+  B <-as.factor(RGB_validation_data$Particle_type)
   CM <- confusionMatrix(data = A, reference = B)
   return(CM)
 }
 
+#PIM
 PIM_validation<-function(Overall_dataset){
   RGB_validation_data<-create_train_test(Overall_dataset, size = 0.8, train=FALSE)
   PIM_RGB_model<-PIM_creation(Overall_dataset)
   predict_unknown<-predict(PIM_RGB_model, RGB_validation_data, type="class")
   A <-as.factor(predict(PIM_RGB_model, RGB_validation_data, type="class"))
-  B <-as.factor(RGB_validation_data$material)
+  B <-as.factor(RGB_validation_data$Particle_type)
   CM <- confusionMatrix(data = A, reference = B)
   return(CM)
 }
@@ -93,46 +100,48 @@ PIM_validation(RGB_dataset)
 #Function to predict the plastic/non-plastic origin of particles using the constructed Plastics Detection Model (PDM),
 #and function to predict the polymer type microplastic particles belong to, using the constructed Polymer Identification Model (PIM).
 
-Inport<-setwd("Location_of_RGB-datasets_with_unknown_particles_which_are_to_be_identified")
+Import<-setwd("Location_of_RGB-datasets_with_unknown_particles_which_are_to_be_identified")
 Export <-setwd("Location_where_particle_prediction_xlsx-file_should_be_stored")
 
-PDM_prediction<-function(Overall_dataset){  
-  files<-list.files(path = "Path_where_your_RGB_datasets_of_unknown_particles_are_stored", pattern=".xlsx")
+#PDM
+PDM_prediction<-function(Location_unknown_datasets){
+  traindataset<-create_train_test(RGB_dataset, size = 0.8, train=TRUE)
+  model<-PDM_creation(traindataset)  
+  files<-list.files(path = Location_unknown_datasets, pattern=".xlsx")
   for (f in files) {
-    read.xlsx(xlsxFile = f, sheet = 1)
+    Prediction_df<-read.xlsx( paste0(Import,f), sheetIndex = 1)
     fnstring<-(as.character(as.filename(f)))
     Filename<- paste(substr(fnstring,1,nchar(fnstring)-5),"_Predicted_IDs",".xlsx",sep= "")
     Exportdirectory=paste(Export,"/",Filename,sep="")
-    traindataset<-create_train_test(RGB_dataset, size = 0.8, train=TRUE)
-    model<-PDM_creation(traindataset)
     Predicted_particle_IDs=c("Particle_ID","Predicted_origin")
-    for (i in 1:nrow(f)){
-      subdataset<-f[2:37]
+    for (i in 1:nrow(Prediction_df)){
+      subdataset<-Prediction_df[2:37]
       predicted_ID<-predict(model,subdataset[i,],type='class')
-      newrow=c(subdataset[i,1],predicted_ID)
+      newrow=c(Prediction_df[i,1],predicted_ID)
       Predicted_particle_IDs<-rbind(Predicted_particle_IDs,newrow)
-      write.xlsx(Predicted_particle_IDs,file = Exportdirectory,sheetName="Sheet1",row.names = FALSE)
     }
+    write.xlsx(Predicted_particle_IDs,file = Exportdirectory,sheetName="Sheet1",row.names = FALSE)
   }
 }
 
-PIM_prediction<-function(Overall_dataset){  
-  files<-list.files(path = "Path_where_your_RGB_datasets_of_unknown_particles_are_stored", pattern=".xlsx")
+#PIM
+PIM_prediction<-function(Location_unknown_datasets){
+  traindataset<-create_train_test(RGB_dataset, size = 0.8, train=TRUE)
+  model<-PIM_creation(traindataset)  
+  files<-list.files(path = Location_unknown_datasets, pattern=".xlsx")
   for (f in files) {
-    read.xlsx(xlsxFile = f, sheet = 1)
+    Prediction_df<-read.xlsx( paste0(Import,f), sheetIndex = 1)
     fnstring<-(as.character(as.filename(f)))
     Filename<- paste(substr(fnstring,1,nchar(fnstring)-5),"_Predicted_IDs",".xlsx",sep= "")
     Exportdirectory=paste(Export,"/",Filename,sep="")
-    traindataset<-create_train_test(RGB_dataset, size = 0.8, train=TRUE)
-    model<-PIM_creation(traindataset)
     Predicted_particle_IDs=c("Particle_ID","Predicted_origin")
-    for (i in 1:nrow(f)){
-      subdataset<-f[2:37]
+    for (i in 1:nrow(Prediction_df)){
+      subdataset<-Prediction_df[2:37]
       predicted_ID<-predict(model,subdataset[i,],type='class')
-      newrow=c(subdataset[i,1],predicted_ID)
+      newrow=c(Prediction_df[i,1],predicted_ID)
       Predicted_particle_IDs<-rbind(Predicted_particle_IDs,newrow)
-      write.xlsx(Predicted_particle_IDs,file = Exportdirectory,sheetName="Sheet1",row.names = FALSE)
     }
+    write.xlsx(Predicted_particle_IDs,file = Exportdirectory,sheetName="Sheet1",row.names = FALSE)
   }
 }
 
